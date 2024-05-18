7a8e729c042c74ea2e478350abb97954fcba0877
NehaW4 <nehuwakhare04@gmail.com>
2024-05-18 13:56:57 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import UseState from './hooks/useState';
+import UseEffect from './hooks/useEffect';
+import UseReducer from './hooks/useReducer';
+
+const App = () => {
+  return (
+  
+    // <UseState/>
+       <UseEffect/>
+    // <UseReducer/>
+  )
+}
+
+export default App;
+
